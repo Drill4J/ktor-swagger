005b0d4901dd40662f642fc94575620acb117d98
ViktoriiaLebedeva <viktoriia_lebedeva@epam.com>
2020-02-11 15:43:43 +0300

    EPMDJ-2364. Update ktor version to 1.3.0.

--- ktor-sample-swagger/ktor-sample-swagger.gradle.kts
+++ ktor-sample-swagger/ktor-sample-swagger.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 fun DependencyHandler.ktor(name: String) =
-    create(group = "io.ktor", name = name, version = "1.3.0-rc")
+    create(group = "io.ktor", name = name, version = "1.3.0")
 
 dependencies {
     implementation(project(":ktor-swagger"))
